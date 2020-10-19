a090ec329856b16b02feb638e746e34d2a462209
Alex <alex.d.bondarev@gmail.com>
2020-10-19 20:15:52 +0300

    try verify api

--- src/test/java/org/test/voice/TestVonageAPI.java
+++ src/test/java/org/test/voice/TestVonageAPI.java
@@ -4,6 +4,10 @@ import com.nexmo.client.NexmoClient;
 import com.nexmo.client.sms.SmsSubmissionResponse;
 import com.nexmo.client.sms.SmsSubmissionResponseMessage;
 import com.nexmo.client.sms.messages.TextMessage;
+import com.nexmo.client.verify.VerifyClient;
+import com.nexmo.client.verify.VerifyRequest;
+import com.nexmo.client.verify.VerifyResponse;
+import com.nexmo.client.verify.VerifyStatus;
 import com.nexmo.client.voice.Call;
 import com.nexmo.client.voice.CallEvent;
 import com.nexmo.client.voice.VoiceName;
@@ -55,5 +59,46 @@ public class TestVonageAPI {
         for (SmsSubmissionResponseMessage responseMessage : response.getMessages()) {
             System.out.println(responseMessage);
         }
+    }
+
+    @Ignore
+    @Test
+    public void testVerify() {
+        boolean cancelRequest = true;
+
+        NexmoClient client = new NexmoClient.Builder() // "new" was missing
+                .apiKey("find your own key")
+                .apiSecret("find your own secret")
+                .build();
+        VerifyClient verifyClient = client.getVerifyClient();
+
+        if (!cancelRequest) {
+            // "deprecated" code in getting started guide?
+            // What should I use instead?
+            // Unclear from code source.
+            VerifyRequest request = new VerifyRequest("find your own phone", "Vonage");
+            request.setLength(4);
+
+            VerifyResponse response = verifyClient.verify(request);
+
+            if (response.getStatus() == VerifyStatus.OK) {
+                System.out.printf("RequestID: %s", response.getRequestId());
+                // RequestID: 9f88b52274de4c4fb14345450612ee4d
+                // Why did this pass?
+                // I got the code "6091" only 2 minutes after this test was executed.
+                // It should have failed.
+                // In 2 minutes I got the next code 0_o "4144"
+            } else {
+                System.out.printf("ERROR! %s: %s",
+                        response.getStatus(),
+                        response.getErrorText()
+                );
+            }
+        }
+
+        verifyClient.cancelVerification("9f88b52274de4c4fb14345450612ee4d");
+        // Status 19: Verification request  ['9f88b52274de4c4fb14345450612ee4d'] can't be cancelled now.
+        // Too many attempts to re-deliver have already been made.
+        System.out.println("Verification cancelled.");
     }
 }
