6b810024645f8f9646b10f62e6cb49698596ade9
Alex <alex.d.bondarev@gmail.com>
2020-10-19 20:01:26 +0300

    try sms api

--- src/test/java/org/test/voice/TestVonageAPI.java
+++ src/test/java/org/test/voice/TestVonageAPI.java
@@ -1,20 +1,25 @@
 package org.test.voice;
 
 import com.nexmo.client.NexmoClient;
+import com.nexmo.client.sms.SmsSubmissionResponse;
+import com.nexmo.client.sms.SmsSubmissionResponseMessage;
+import com.nexmo.client.sms.messages.TextMessage;
 import com.nexmo.client.voice.Call;
 import com.nexmo.client.voice.CallEvent;
 import com.nexmo.client.voice.VoiceName;
 import com.nexmo.client.voice.ncco.Ncco;
 import com.nexmo.client.voice.ncco.TalkAction;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestVonageAPI {
 
+    @Ignore
     @Test
     public void testVoice() {
         NexmoClient client = NexmoClient.builder()
-                .applicationId("c059d7ba-cd21-4bcc-91b3-91d32eae7de7")
-                .privateKeyPath("/Users/Alex/.ssh/keys/vonage/private.key")
+                .applicationId("find your own app id")
+                .privateKeyPath("find your own key")
                 .build();
 
         Ncco ncco = new Ncco(
@@ -24,8 +29,8 @@ public class TestVonageAPI {
                         .build()
         );
 
-        String TO_NUMBER = "380931087097";
-        String FROM_NUMBER = "380931087097";
+        String TO_NUMBER = "find your own phone";
+        String FROM_NUMBER = "find your own phone";
 
         CallEvent result = client
                 .getVoiceClient()
@@ -34,5 +39,21 @@ public class TestVonageAPI {
         System.out.println(result.getConversationUuid());
     }
 
+    @Ignore
+    @Test
+    public void testSMS() {
+        NexmoClient client = new NexmoClient.Builder()
+                .apiKey("find your own key")
+                .apiSecret("find your own secret")
+                .build();
 
+        String messageText = "Hello from Vonage SMS API";
+        TextMessage message = new TextMessage("Vonage APIs", "find your own phone", messageText);
+
+        SmsSubmissionResponse response = client.getSmsClient().submitMessage(message);
+
+        for (SmsSubmissionResponseMessage responseMessage : response.getMessages()) {
+            System.out.println(responseMessage);
+        }
+    }
 }
